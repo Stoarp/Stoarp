61c7bab722e10e247089ad2deff5aea2c2cf22e2
ExilProductions <exil.productions.business@gmail.com>
2026-02-14 18:39:55 +0100

    Project Start

---
+++ Stoarp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Stoarp.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Stoarp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Stoarp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
