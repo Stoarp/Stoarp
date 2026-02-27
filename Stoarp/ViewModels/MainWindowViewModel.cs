4c6df354505dc68eb73db937b0d42c9b4ea107d9
ExilProductions <exil.productions.business@gmail.com>
2026-02-27 08:43:17 +0100

    Add StoatSharp Client as Submodule

--- Stoarp/ViewModels/MainWindowViewModel.cs
+++ Stoarp/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace Stoarp.ViewModels;
 
-public partial class MainWindowViewModel : ViewModelBase
+public class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
 }
